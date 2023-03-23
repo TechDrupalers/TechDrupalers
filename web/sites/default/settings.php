524ee8eb9b9f3a21e13a8edae4094ad7734f530c
Karan <karanthorat7757@gmail.com>
2023-03-23 12:16:41 +0530

    composer lock and json

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -827,3 +827,9 @@ $databases['default']['default'] = array (
   'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
 );
 $settings['config_sync_directory'] = '../config';
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
